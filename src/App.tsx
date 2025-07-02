5ebb8bcf81cfd5c0c10c265b83c5ece2214c9ba4
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-07-02 15:51:47 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-amber-300 text-9xl w-full "> aryan love nancy !!! </h1>
+    </>
+  );
+}
+
+export default App;
