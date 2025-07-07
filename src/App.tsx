912c67413a584312d08d385c8bcefde6b4076427
Aryan <aryanmail561@gmail.com>
2025-07-07 14:44:14 +0530

    feat: refactor App and main components to integrate routing with Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
-import "./App.css";
-import { services } from "./services/services";
-
-function App() {
-  const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["programme-result", "bca", "2023"],
-    queryFn: () => services.getProgrammeResult("bca", "2023"),
-    enabled: false, // only fetch on button click
-  });
+import type { FC } from "react";
+// import { useQuery } from "@tanstack/react-query";
+// import { services } from "./services/services";
+import Footer from "./component/Footer/Footer";
+import Navbar from "./component/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+const App: FC = () => {
+  // const { data, isLoading, error, refetch } = useQuery({
+  //   queryKey: ["programme-result", "bca", "2023"],
+  //   queryFn: () => services.getProgrammeResult("bca", "2023"),
+  //   enabled: false,
+  // });
 
   return (
     <>
-      <h1 className="bg-amber-300 text-9xl w-full">tan stack !!!</h1>
-      <button onClick={() => refetch()}>click</button>
-
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {(error ).message}</p>}
-      {data && data.status !== "error" && (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      )}
+      <div className="min-h-screen w-full box-border p-2 bg-gradient-to-br from-green-100 to-lime-100 text-white"> 
+        <Navbar/>
+        <Outlet/>
+        <Footer />
+      </div>
     </>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home.tsx";
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children:[
+      {
+        path:'/',
+      element:<Home/> ,
+    }
+    ]
+  }
+])
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>
 );
