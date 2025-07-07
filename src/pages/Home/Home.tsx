4cc0d5f485a5ab92465c633b431890a384b8093d
Aryan <aryanmail561@gmail.com>
2025-07-07 14:44:02 +0530

    feat: create Home component

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl text-green-800 font-bold mb-4">Welcome to KIHEAT Ranklist</h1>
+            <p className="text-lg text-gray-700">Your one-stop solution for all ranklist needs.</p>
+        </div>
+    );
+}
+
+export default Home;
