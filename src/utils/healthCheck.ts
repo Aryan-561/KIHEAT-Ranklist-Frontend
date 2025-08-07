f7dfee73ec630bba445886c1b45231b853adcba6
Aryan-561 <aryanmail561@gmail.com>
2025-08-08 00:08:40 +0530

    fix: update health check method to use axios instance

--- src/utils/healthCheck.ts
+++ src/utils/healthCheck.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from "./axios";
 // src/utils/healthCheck.ts
 export const checkHealth = async () => {
   try {
-    await fetch(`${axiosInstance}/health`);
+    await axiosInstance.get('/health')
     console.log('✅ Health check successful');
   } catch (err) {
     console.error('❌ Health check failed', err);
